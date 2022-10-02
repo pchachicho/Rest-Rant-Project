a3e3759e9e7a0c6ca7e8dc1f09df5e66eda8e322
Patrick hachicho <Hachichopatrick@gmail.com>
2022-10-02 16:13:25 -0400

    Adding places controller

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use('places', require('../Rest-Rant/controllers/places'))
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
