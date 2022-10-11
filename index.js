ff14de9061b56ea8a85b260af0f56a42b585032d
Patrick hachicho <Hachichopatrick@gmail.com>
2022-10-10 20:16:37 -0400

    added bootstrap

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Modules and Globals
 require("dotenv").config();
 const express = require("express");
 const app = express();
 
+// Express Settings
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
-
 app.use("/places", require("../Rest-Rant/controllers/places"));
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
