f661a95dc6a71e9e5191291512990248fb638281
Patrick hachicho <Hachichopatrick@gmail.com>
2022-10-16 21:18:30 -0400

    New Places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 router.get("/new", (req, res) => {
   res.render("places/new");
@@ -23,8 +24,18 @@ router.get("/", (req, res) => {
   res.render("places/index", { places });
 });
 router.post("/", (req, res) => {
-  console.log(req.body)
-  res.send("POST /places");
+  console.log(req.body);
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = "USA"
+  }
+  places.push(req.body)
+  res.redirect("/places");
 });
 
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    name: "Mi Cancun",
+    city: "Cary",
+    state: "NC",
+    cuisines: "Mexican",
+    pic: "/images/Cary.jpeg",
+  },
+  {
+    name: "Taziki's Mediterranean Cafe",
+    city: "Cary",
+    state: "NC",
+    cuisines: "Mediterranean, Cafe",
+    pic: "/images/RTP.jpeg",
+  },
+];
